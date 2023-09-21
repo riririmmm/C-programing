4fba857c16c9d6a5d6357447402151129253a7c5
riririmmm <mr8774@smail.kongju.ac.kr>
2023-09-21 16:04:28 +0900

    Create lecture4

--- 0921/0921/lecture3.c
+++ 0921/0921/lecture3.c
@@ -5,10 +5,10 @@ int Fact(int a);
 int main(void) {
 	int num;
 
-	printf("수를 입력하세요: ");
+	printf("계산할 팩토리얼 값: ");
 	scanf_s("%d", &num);
 
-	printf("%d", Fact(num));
+	printf("%d! = %d", num, Fact(num));
 
 	return 0;
 }
@@ -18,5 +18,4 @@ int Fact(int a) {
 		return 1;
 	else
 		a = a * Fact(a - 1);
-	return 0;
 }
