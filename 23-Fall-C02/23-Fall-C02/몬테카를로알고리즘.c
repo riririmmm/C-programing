1d2ec6aab380b70440251d91186c2229aeca56c1
riririmmm <mr8774@smail.kongju.ac.kr>
2023-10-10 20:38:41 +0900

    Create 몬테카를로알고리즘.c

---
+++ 23-Fall-C02/23-Fall-C02/몬테카를로알고리즘.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(void) {
+    double x = 0;
+    double y = 0;
+
+    double count = 0, circle = 0;
+
+    int percent = 0;
+
+    srand(time(NULL));
+
+    while (count < 10000) {
+        x = (double)rand() / (double)RAND_MAX;
+        y = (double)rand() / (double)RAND_MAX;
+        count++;
+
+        if ((x * x) + (y * y) <= 1) {
+            circle++;
+        }
+
+        int newper = count / 100;
+
+        if (newper != percent) {
+            percent = newper;
+            printf("%d%%진행.. 원주율: %lf ", percent, (circle / count) * 4);
+
+            int s = floor(percent / 5);
+
+            for (int i = 0; i < 20; i++) {
+                if (i < s)
+                    printf("■");
+                else
+                    printf("□");
+            }
+            printf("\n");
+        }
+    }
+
+    printf("원주율: %lf", (circle / count) * 4);
+
+    return 0;
+}
