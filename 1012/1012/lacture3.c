f0ef59b1c2dade754150fe1685041ecdcbd02353
riririmmm <mr8774@smail.kongju.ac.kr>
2023-10-12 15:18:01 +0900

    Create lacture3.c

---
+++ 1012/1012/lacture3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int score[5] = { 87, 92, 89, 98, 78 };
+	int cnt;
+
+	printf("score[5] = { 87, 92, 89, 98, 78 } 배열 요소 출력\n");
+
+	for (cnt = 0; cnt < 5; cnt++)
+		printf("score[%d] = [%d]\n", cnt, score[cnt]);
+
+	return 0;
+}
