4be3f3e68c0c67a837101b396d86c730e5976de1
riririmmm <mr8774@smail.kongju.ac.kr>
2023-10-12 14:47:36 +0900

    Create lacture1.c

---
+++ 1012/1012/lacture1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a = 3;
+	char* ptr1 = &a;
+
+	char b = "b";
+	char* ptr2 = &b;
+
+	printf("%d\n", ptr1);
+	printf("%d\n", sizeof(ptr1));
+
+	printf("%d\n", ptr2);
+	printf("%d\n", sizeof(ptr2));
+
+	return 0;
+}
