703eb7b078f4be3e2c6c20fb48f8a3c58e934077
riririmmm <mr8774@smail.kongju.ac.kr>
2023-10-12 15:22:49 +0900

    Update lacture3.c

--- 1012/1012/lacture3.c
+++ 1012/1012/lacture3.c
@@ -4,10 +4,14 @@ int main(void) {
 	int score[5] = { 87, 92, 89, 98, 78 };
 	int cnt;
 
+	int* pscore = score;
+
 	printf("score[5] = { 87, 92, 89, 98, 78 } 배열 요소 출력\n");
 
-	for (cnt = 0; cnt < 5; cnt++)
-		printf("score[%d] = [%d]\n", cnt, score[cnt]);
+	for (cnt = 0; cnt < 5; cnt++) {
+		printf("score[%d] = [%d]\n", cnt, *pscore);
+		pscore++;
+	}
 
 	return 0;
 }
